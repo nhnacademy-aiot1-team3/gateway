226aff70e63f63c71c76e6d1d5f778d20c8962b2
kkh5535 <kkh5535@naver.com>
2024-03-30 16:45:42 +0900

    Rename: package 경로 수정
    com.nhnacademy.gateway -> com.databo3.gateway

--- src/main/java/com/databo3/gateway/GatewayApplication.java
+++ src/main/java/com/databo3/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.nhnacademy.gateway;
+package com.databo3.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
