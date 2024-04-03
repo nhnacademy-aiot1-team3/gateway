b4e64efa4790cc28461d2d34a1136729da8e11b4
kkh5535 <kkh5535@naver.com>
2024-04-03 16:58:46 +0900

    Refactor: RouteValidator 에 AntPathMatcher 추가
    path 에 와일드카드가 포함되어도 인식 할 수 있도록 수정

--- src/main/java/live/databo3/gateway/util/RouteValidator.java
+++ src/main/java/live/databo3/gateway/util/RouteValidator.java
@@ -2,6 +2,7 @@ package live.databo3.gateway.util;
 
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 
 import java.util.List;
 import java.util.function.Predicate;
@@ -19,8 +20,16 @@ public class RouteValidator {
      */
     public static final List<String> NEED_NOT_TOKEN_ENDPOINTS = List.of(
             "/auth/login",
+            "/auth/*/oauth",
             "/api/account/member/register"
     );
+
+    /**
+     * 와일드카드를 포함한 path 관리를 위한 matcher
+     * @since 1.0.1
+     */
+    private final AntPathMatcher pathMatcher = new AntPathMatcher();
+
 
     /**
      * NEED_NOT_TOKEN_ENDPOINTS 와 request 의 uri 를 대조하여 인가가 필요한지 여부를 반환한다.
@@ -29,5 +38,5 @@ public class RouteValidator {
     public Predicate<ServerHttpRequest> isSecured =
             request -> NEED_NOT_TOKEN_ENDPOINTS
                     .stream()
-                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+                    .noneMatch(uri -> pathMatcher.match(uri, request.getURI().getPath()));
 }
