bc828f6d4028a2bd33ecbeffe2e6d1e09d41326c
kkh5535 <kkh5535@naver.com>
2024-05-17 16:44:30 +0900

    Fix: sensorApi routeValidate 추가

--- src/main/java/live/databo3/gateway/util/RouteValidator.java
+++ src/main/java/live/databo3/gateway/util/RouteValidator.java
@@ -24,8 +24,7 @@ public class RouteValidator {
             "/api/account/member/register",
             "/api/account/member/duplicate/*",
             "/api/account/member/searchPassword/*",
-            "/api/account/member/email/*",
-            "/api/sensor/**"
+            "/api/account/member/email/*"
     );
 
     /**
